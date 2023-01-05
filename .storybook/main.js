3603553d6e00a250f69d050cd440c70610d2a37e
Max Kuzkin <maxkuzkin@gmail.com>
2023-01-05 15:14:40 -0800

    Added draft support for the storybook-addon-designs
    In order to avoid copy-pasting content between our Figma specifications and Component documentation, created a link between them. Not ideal, because of an iframe experience, but should be good enough for now.

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+	"storybook-addon-designs"
   ],
   framework: "@storybook/vue3",
   core: {
