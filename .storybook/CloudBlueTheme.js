5a786cfc104c894cb550e3493e3baafd77f3103e
Max Kuzkin <maxkuzkin@gmail.com>
2023-01-05 17:14:59 -0800

    Added minimalistic support for the CloudBlue brand
    Logo, title and base URL are now pointing to CloudBlue.

---
+++ .storybook/CloudBlueTheme.js
@@ -0,0 +1,11 @@
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'CloudBlue Connect',
+  brandUrl: 'https://connect.cloudblue.com',
+  brandImage: 'https://connect.cloudblue.com/wp-content/uploads/2020/07/ClouBlue-Standalone-Logo-Blue.png',
+  brandTarget: '_self',
+});
+
---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+import cloudBlueTheme from './CloudBlueTheme';
+
+addons.setConfig({
+  theme: cloudBlueTheme,
+});
