62a3547e39cccbddde5dad4d0138568f63b8e7d9
Arnau Giralt <arnau.giralt@cloudblue.com>
2023-07-21 16:49:21 +0200

    LITE-28158: Fix imports on the `injector/core` files, delete unused `fromKeys` helper

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,5 +1,3 @@
-export const fromKeys = (ks) => ks.reduce((acc, k) => { acc[k] = null; return acc; }, {});
-
 export const clone = v => JSON.parse(JSON.stringify(v));
 
 export const has = (prop, obj) => Object.keys(obj).includes(prop);
