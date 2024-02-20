80b0d6cf688cf7f9a94dafa58442681875cc3329
Arnau Giralt <50662025+arnaugiralt@users.noreply.github.com>
2024-02-20 12:06:41 +0100

    Add alignment options to Menu component (#50)

--- components/src/widgets/menu/widget.vue
+++ components/src/widgets/menu/widget.vue
@@ -3,17 +3,18 @@
     ref="menu"
     class="menu"
   >
-    <div 
-      class="menu-trigger" 
+    <div
+      class="menu-trigger"
       @click.stop="toggle"
     >
       <slot name="trigger" />
     </div>
-    
+
     <div class="menu-content-wrapper">
-      <div 
+      <div
         v-if="showMenu"
         class="menu-content"
+        :class="alignmentClass"
         @click.stop
       >
         <slot name="content" />
@@ -23,39 +24,59 @@
 </template>
 
 <script setup>
-  import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, computed } from 'vue'
 
-  const showMenu = ref(false)
-  const menu = ref(null)
+const props = defineProps({
+  align: {
+    type: String,
+    default: 'left',
+    validator(value) {
+      return ['left', 'right'].includes(value);
+    },
+  },
+});
 
-  const toggle = () => {
-    showMenu.value = !showMenu.value;
+const showMenu = ref(false);
+const menu = ref(null);
+
+const alignmentClass = computed(() => (props.align === 'left'
+  ? 'menu-content_align-left'
+  : 'menu-content_align-right'
+));
+
+const toggle = () => {
+  showMenu.value = !showMenu.value;
+};
+
+const handleClickOutside = (event) => {
+  if (menu.value && !menu.value.contains(event.target)) {
+    showMenu.value = false;
   }
+};
 
-  const handleClickOutside = (event) => {
-    if (menu.value && !menu.value.contains(event.target)) {
-      showMenu.value = false;
-    }
-  }
+onMounted(() => {
+  document.addEventListener("click", handleClickOutside);
+});
 
-  onMounted(() => {
-    document.addEventListener("click", handleClickOutside)
-  })
-
-  onUnmounted(() => {
-    document.removeEventListener("click", handleClickOutside)
-  })
+onUnmounted(() => {
+  document.removeEventListener("click", handleClickOutside);
+});
 </script>
 
 <style lang="stylus" scoped>
+.menu-content-wrapper {
+  position: relative;
+}
 
-  .menu-content-wrapper {
-    position: relative;
+.menu-content {
+  position: absolute;
+  top: 0;
+
+  &_align-right {
+    right: 0;
   }
-
-  .menu-content {
-    position: absolute;
-    top: 0;
+  &_align-left {
     left: 0;
   }
+}
 </style>
