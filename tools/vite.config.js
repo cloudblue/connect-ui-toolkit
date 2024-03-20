d1f251a393d65f42ef3d1c3efbadded038e12bcb
Paulina Marcinowska <paulina.marcinowska@cloudblue.com>
2024-03-20 16:22:34 +0100

    LITE-29720 create vite config in tools

---
+++ tools/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+       "fastApi/adapter": resolve(__dirname, 'api/fastApi/adapter.js'),
+       "fastApi/vue-composable": resolve(__dirname, 'api/fastApi/vue-composable.js'),
+       "build/vite": resolve(__dirname, 'build/vite/index.js'),
+       "vite/toolkit": resolve(__dirname, 'vue/toolkit.js')
+      },
+      fileName: (_, entryName) => {
+        return `tools/${entryName}.js`;
+      },
+    },
+    rollupOptions: {
+      output: {
+        dir: resolve(__dirname, '..', 'dist'),
+      },
+      external: [
+       'vue',
+        /node:\w*/,
+      ],
+    },
+  },
+})
