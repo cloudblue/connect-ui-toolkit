226f6f947e694da7b9122b59337ef1a724ce0db2
Paulina Marcinowska <paulina.marcinowska@cloudblue.com>
2024-06-05 12:59:44 +0200

    Delete unused icons, update story

--- components/src/stories/Icon.stories.js
+++ components/src/stories/Icon.stories.js
@@ -3,10 +3,6 @@ import registerWidget from '~core/registerWidget';
 
 import * as iconsAnimated from '@cloudblueconnect/material-svg/animated';
 import * as iconsBaseline from '@cloudblueconnect/material-svg/baseline';
-import * as iconsOutline from '@cloudblueconnect/material-svg/outline';
-import * as iconsRound from '@cloudblueconnect/material-svg/round';
-import * as iconsSharp from '@cloudblueconnect/material-svg/sharp';
-import * as iconsTwotone from '@cloudblueconnect/material-svg/twotone';
 
 registerWidget('ui-icon', Icon);
 
@@ -51,7 +47,7 @@ export const Animated = {
       Object.keys(iconsAnimated)
         .map(
           (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
+            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="24"/> <span>${icon}</span></div>`,
         )
         .join('') +
       '</div>',
@@ -75,103 +71,7 @@ export const Baseline = {
       Object.keys(iconsBaseline)
         .map(
           (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
-        )
-        .join('') +
-      '</div>',
-  }),
-  args: {},
-  argTypes: {
-    iconName: { control: false },
-    size: { control: false },
-    color: { control: false },
-  },
-};
-
-export const Outline = {
-  name: 'All outline',
-  render: () => ({
-    setup() {
-      return {};
-    },
-    template:
-      '<div className="sb-all-icons">' +
-      Object.keys(iconsOutline)
-        .map(
-          (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
-        )
-        .join('') +
-      '</div>',
-  }),
-  args: {},
-  argTypes: {
-    iconName: { control: false },
-    size: { control: false },
-    color: { control: false },
-  },
-};
-
-export const Round = {
-  name: 'All round',
-  render: () => ({
-    setup() {
-      return {};
-    },
-    template:
-      '<div className="sb-all-icons">' +
-      Object.keys(iconsRound)
-        .map(
-          (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
-        )
-        .join('') +
-      '</div>',
-  }),
-  args: {},
-  argTypes: {
-    iconName: { control: false },
-    size: { control: false },
-    color: { control: false },
-  },
-};
-
-export const Sharp = {
-  name: 'All sharp',
-  render: () => ({
-    setup() {
-      return {};
-    },
-    template:
-      '<div className="sb-all-icons">' +
-      Object.keys(iconsSharp)
-        .map(
-          (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
-        )
-        .join('') +
-      '</div>',
-  }),
-  args: {},
-  argTypes: {
-    iconName: { control: false },
-    size: { control: false },
-    color: { control: false },
-  },
-};
-
-export const Twotone = {
-  name: 'All twotone',
-  render: () => ({
-    setup() {
-      return {};
-    },
-    template:
-      '<div className="sb-all-icons">' +
-      Object.keys(iconsTwotone)
-        .map(
-          (icon) =>
-            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="32"/> <span>${icon}</span></div>`,
+            `<div className="sb-icon-wrapper"><ui-icon className="sb-icon" iconName="${icon}" size="24"/> <span>${icon}</span></div>`,
         )
         .join('') +
       '</div>',
